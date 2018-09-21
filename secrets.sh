13c2d5252189afdd83ecd1112ffd04e8b0a599c6
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-21 13:21:44 -0700

    revision 1

---
+++ secrets.sh
@@ -0,0 +1,7 @@
+# Secrets for your Eventbrite app
+# Find these at: https://www.eventbrite.ca/account-settings/apps
+# This file should never go into github!
+
+export EVENTBRITE_KEY="U7E53N3DIYULXDWFQV"
+export EVENTBRITE_SECRET="VZFTAYTJRLNIKDPTNIMFHWRERCSEV7AMQHMZL2TIVBZ6N22X75"
+export EVENTBRITE_TOKEN="WOUN3GGOE4AWHTH4GYZH"
